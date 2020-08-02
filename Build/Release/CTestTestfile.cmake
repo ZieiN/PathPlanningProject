5aa988d356159ca3c7815a32efcfa9cda11de9e4
Zain <zainalabedeenali@hotmail.com>
2020-08-02 22:30:21 +0300

    deleter build files

--- Build/Release/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zain/PathPlanningProject
-# Build directory: /home/zain/PathPlanningProject/Build/Release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Src")
-subdirs("Tests")
